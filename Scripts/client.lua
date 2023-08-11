9fba8f7b2f0565b0e44796bd45ef3bdba71f49b6
DaNussi <daniel.nussbaum@htlstp.at>
2023-08-12 00:19:27 +0200

    as

--- Scripts/client.lua
+++ Scripts/client.lua
@@ -1,11 +1,10 @@
-local bridge = peripheral.find("rsBridge")
-local items = bridge.listItems()
-
 function bool_to_number(value)
     return value and 1 or 0
 end
 
-local body = "measurement,item=host1 field1=2i,field2=2.0";
+local bridge = peripheral.find("rsBridge")
+local items = bridge.listItems()
+local body = "";
 
 for index, item in pairs(items) do
     local name = item["name"]
@@ -16,7 +15,9 @@ for index, item in pairs(items) do
     itemData = itemData .. "item="..name.."_"..index
     itemData = itemData .. " "
     itemData = itemData .. "name="..name
+    itemData = itemData .. ","
     itemData = itemData .. "amount="..amount
+    itemData = itemData .. ","
     itemData = itemData .. "isCraftable="..bool_to_number(isCraftable)
 
     print(itemData)
@@ -28,9 +29,9 @@ local headers = {
     [ "Authorization" ] = "Token IYW-b_B1ImVPW1i09j4nafgkv4z1cCQdQn_P04JULaYdWgHNjn5JH883cczJIPrxuZg3w7nmkiPl0kyS7DuTXA=="
 }
 
-local request = http.request{
-                    url = "https://influx.nussi.net/write?db=rf",
-                    body = body,
-                    headers = headers,
-                    method = "POST"
-                }
+http.request{
+    url = "https://influx.nussi.net/write?db=rf",
+    body = body,
+    headers = headers,
+    method = "POST"
+}
