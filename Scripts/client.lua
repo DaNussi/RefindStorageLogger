502dfa66d55f9988d4458ad4b0e42a3d69263bcb
DaNussi <daniel.nussbaum@htlstp.at>
2023-08-11 22:58:34 +0200

    LaCommit

--- Scripts/client.lua
+++ Scripts/client.lua
@@ -1,5 +1,2 @@
-local bridge = peripheral.find("rsBridge")
-
-local request = http.get("http://192.168.0.106:8086/metrics")
-local data = request.readAll()
-print(data)
+# local bridge = peripheral.find("rsBridge")
+print(http.get("http://192.168.0.106:8086/metrics").readAll())
