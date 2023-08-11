2955f11266560cc179a290293ae25d3198e5562b
DaNussi <daniel.nussbaum@htlstp.at>
2023-08-11 23:46:39 +0200

    as

--- Scripts/client.lua
+++ Scripts/client.lua
@@ -2,8 +2,14 @@ local bridge = peripheral.find("rsBridge")
 local items = bridge.listItems()
 
 for index, item in pairs(items) do
+
+    local name = item["name"]
+    local amount = item["amount"]
+    local isCraftable = item["isCraftable"]
+    
+
     if(index == 1) then
-        for k,v in pairs(item) do
+        for k,v in pairs(item["nbt"]) do
             print(tostring(k)..' = '..tostring(v))
         end
     end
