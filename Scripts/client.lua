9d72c910210a26002b607f7e16fe80f3645d52f6
DaNussi <daniel.nussbaum@htlstp.at>
2023-08-12 00:03:43 +0200

    as

--- Scripts/client.lua
+++ Scripts/client.lua
@@ -1,16 +1,22 @@
 local bridge = peripheral.find("rsBridge")
 local items = bridge.listItems()
 
-for index, item in pairs(items) do
+local body = "measurement,item=host1 field1=2i,field2=2.0";
 
+for index, item in pairs(items) do
     local name = item["name"]
     local amount = item["amount"]
     local isCraftable = item["isCraftable"]
-    
 
-    if(index == 1) then
-        for k,v in pairs(item["nbt"]) do
-            print(tostring(k)..' = '..tostring(v))
-        end
-    end
 end
+
+local headers = {
+    [ "Authorization" ] = "Token IYW-b_B1ImVPW1i09j4nafgkv4z1cCQdQn_P04JULaYdWgHNjn5JH883cczJIPrxuZg3w7nmkiPl0kyS7DuTXA=="
+}
+
+local request = http.request{
+                    url = "https://influx.nussi.net/write?db=rf",
+                    body = body,
+                    headers = headers,
+                    method = "POST"
+                }
