133d6ae0a32db95cdb51c755ea66f4e53d656a86
DaNussi <daniel.nussbaum@htlstp.at>
2023-08-12 01:23:15 +0200

    a

--- Scripts/client.lua
+++ Scripts/client.lua
@@ -1,32 +1,37 @@
-function bool_to_number(value)
-    return value and 1 or 0
+while true do
+    function bool_to_number(value)
+        return value and 1 or 0
+    end
+    
+    local bridge = peripheral.find("rsBridge")
+    local items = bridge.listItems()
+    local body = "";
+    
+    for index, item in pairs(items) do
+        local name = item["name"]
+        local amount = item["amount"]
+        local isCraftable = item["isCraftable"]
+    
+        local itemData = "measurement,";
+        itemData = itemData .. "item=\""..name.."_"..index.."\" "
+        itemData = itemData .. "name=\""..name.."\","
+        itemData = itemData .. "amount="..amount..","
+        itemData = itemData .. "isCraftable="..bool_to_number(isCraftable)
+    
+        -- print(itemData)
+    
+        body = body..itemData.."\n";
+    end
+    
+    local headers = {
+        [ "Authorization" ] = "Token IYW-b_B1ImVPW1i09j4nafgkv4z1cCQdQn_P04JULaYdWgHNjn5JH883cczJIPrxuZg3w7nmkiPl0kyS7DuTXA=="
+    }
+    
+    request = http.post("https://influx.nussi.net/write?db=rf", body, headers)
+    -- print("=== REQUEST ===")
+    -- print(request)
+    request.close()
+
+    print("Sent data!")
+    os.sleep( 3 )
 end
-
-local bridge = peripheral.find("rsBridge")
-local items = bridge.listItems()
-local body = "";
-
-for index, item in pairs(items) do
-    local name = item["name"]
-    local amount = item["amount"]
-    local isCraftable = item["isCraftable"]
-
-    local itemData = "measurement,";
-    itemData = itemData .. "item=\""..name.."_"..index.."\" "
-    itemData = itemData .. "name=\""..name.."\","
-    itemData = itemData .. "amount="..amount..","
-    itemData = itemData .. "isCraftable="..bool_to_number(isCraftable)
-
-    print(itemData)
-
-    body = body..itemData.."\n";
-end
-
-local headers = {
-    [ "Authorization" ] = "Token IYW-b_B1ImVPW1i09j4nafgkv4z1cCQdQn_P04JULaYdWgHNjn5JH883cczJIPrxuZg3w7nmkiPl0kyS7DuTXA=="
-}
-
-request = http.post("https://influx.nussi.net/write?db=rf", body, headers)
-print("=== REQUEST ===")
-print(request)
-request.close()
