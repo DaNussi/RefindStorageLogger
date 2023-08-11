51c370c0a2474f1384dbcde234adf808ee4c5d38
DaNussi <daniel.nussbaum@htlstp.at>
2023-08-11 22:56:27 +0200

    LaMessage

--- Scripts/client.lua
+++ Scripts/client.lua
@@ -1,3 +1,5 @@
 local bridge = peripheral.find("rsBridge")
 
-print("Bridge")
+local request = http.get("http://192.168.0.106:8086/metrics")
+local data = request.readAll()
+print(data)
--- Scripts/update.lua
+++ Scripts/update.lua
@@ -1,14 +1,19 @@
+print("Fetching")
 local request = http.get("https://raw.githubusercontent.com/DaNussi/RefindStorageLogger/master/Scripts/client.lua")
 local data = request.readAll()
 local fileName = "client.lua"
 
 if fs.exists(fileName) then
+    print("Deleting existing file")
     fs.delete(fileName)
 end
 
+print("Writeing file")
 local file = fs.open(fileName, "w")
 file.writeLine(data)
 file.close() -- never forget to close a file!
 
 -- => HTTP is working!
 request.close()
+
+print("bye")
