aede098153c34fb8bc86ff959cd958748a16c6a7
DaNussi <daniel.nussbaum@htlstp.at>
2023-08-14 17:31:52 +0200

    asa

--- Scripts/client.lua
+++ Scripts/client.lua
@@ -1,28 +1,13 @@
-while true do
-    function bool_to_number(value)
-        return value and 1 or 0
-    end
-    
-    local bridge = peripheral.find("rsBridge")
-    local items = bridge.listItems()
-    local body = "";
-    
-    for index, item in pairs(items) do
-        local name = item["name"]
-        local amount = item["amount"]
-        local isCraftable = item["isCraftable"]
-    
-        local itemData = "measurement,";
-        itemData = itemData .. "item=\""..name.."_"..index.."\" "
-        itemData = itemData .. "name=\""..name.."\","
-        itemData = itemData .. "amount="..amount..","
-        itemData = itemData .. "isCraftable="..bool_to_number(isCraftable)
-    
-        -- print(itemData)
-    
-        body = body..itemData.."\n";
-    end
-    
+
+function bool_to_number(value)
+    return value and 1 or 0
+end
+
+function itemToMeasurement(item)
+    return "measurement,item=\""..item["name"].."\" name=\""..item["name"].."\",amount="..item["amount"]..",isCraftable="..bool_to_number(item["isCraftable"]);
+end
+
+function sendData(body)
     local headers = {
         [ "Authorization" ] = "Token IYW-b_B1ImVPW1i09j4nafgkv4z1cCQdQn_P04JULaYdWgHNjn5JH883cczJIPrxuZg3w7nmkiPl0kyS7DuTXA=="
     }
@@ -30,8 +15,39 @@ while true do
     request = http.post("https://influx.nussi.net/write?db=rf", body, headers)
     -- print("=== REQUEST ===")
     -- print(request)
-    request.close()
 
     print("Sent data!")
+end
+
+
+
+local bridge = peripheral.find("rsBridge")
+while true do
+    
+    local data = bridge.listItems()
+    local items = {}
+
+    for index, item in pairs(data) do
+        local index = item["name"]
+
+        current = items[index]
+        if current == nil then
+            current = item
+        else
+            current["amount"] = current["amount"] + item["amount"]
+        end
+        items[index] = current
+
+
+    end
+
+
+    local body = ""
+    for name, item in pairs(items) do
+        body = body..itemToMeasurement(item).."\n"
+    end
+
+    sendData(body)
+    
     os.sleep( 3 )
 end
